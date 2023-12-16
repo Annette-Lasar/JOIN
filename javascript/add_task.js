2c8d0f69375fb6b6e9fcd296788c7711ce48c151
Annette-Lasar <a_lasar@web.de>
2023-12-16 15:26:38 +0100

    Dropdown lists for categories and contacts and general layout of add tasks page

--- javascript/add_task.js
+++ javascript/add_task.js
@@ -1,6 +1,13 @@
-function toggleCategories() {
-    const CATEGORY_List = document.getElementById('category_list');
-    const SELECT_ARROW = document.getElementById('select_arrow');
+/**
+ * Toggles dropdown lists on add tasks page by displaying all available options
+ * of the dropdown list and rotating the arrow 180 deg
+ * Shows an option to add a new task category resp a new contact
+ * @param {string} idContainer - represents the container that holds the list of options
+ * @param {string} idArrow  - represents the img element with the arrow
+ */
+function toggleDropdownLists(idContainer, idArrow) {
+    const CATEGORY_List = document.getElementById(idContainer);
+    const SELECT_ARROW = document.getElementById(idArrow);
     CATEGORY_List.classList.toggle('show');
     SELECT_ARROW.classList.toggle('turn');
   }
