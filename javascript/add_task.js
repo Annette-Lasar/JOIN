b4499a399c6306c096aef513aaed7deb79729e37
Annette-Lasar <a_lasar@web.de>
2023-12-17 21:35:11 +0100

    Inputfield add subtask with plus button

--- javascript/add_task.js
+++ javascript/add_task.js
@@ -1,3 +1,9 @@
+function initTasks() {
+  init();
+  showAndHideBoxesAccordingToScreenSize();
+  checkforAddTaskPage();
+}
+
 /**
  * Toggles dropdown lists on add tasks page by displaying all available options
  * of the dropdown list and rotating the arrow 180 deg
@@ -6,8 +12,46 @@
  * @param {string} idArrow  - represents the img element with the arrow
  */
 function toggleDropdownLists(idContainer, idArrow) {
-    const CATEGORY_List = document.getElementById(idContainer);
-    const SELECT_ARROW = document.getElementById(idArrow);
-    CATEGORY_List.classList.toggle('show');
-    SELECT_ARROW.classList.toggle('turn');
+  const CATEGORY_List = document.getElementById(idContainer);
+  const SELECT_ARROW = document.getElementById(idArrow);
+  CATEGORY_List.classList.toggle('show');
+  SELECT_ARROW.classList.toggle('turn');
+}
+
+
+function showAndHideBoxesAccordingToScreenSize() {
+  let prioSmallScreen = document.getElementById('prio_small_screen');
+  let dueDateSmallScreen = document.getElementById('due_date_small_screen');
+  let categorySmallScreen = document.getElementById('category_small_screen');
+  let subTasksSmallScreen = document.getElementById('sub_tasks_small_screen');
+  let bigScreen = document.getElementById('form_big_screen');
+  let windowSize = window.innerWidth;
+
+  if (windowSize < 800) {
+    prioSmallScreen.style.display = 'block';
+    dueDateSmallScreen.style.display = 'flex';
+    categorySmallScreen.style.display = 'block';
+    subTasksSmallScreen.style.display = 'flex';
+    bigScreen.style.display = 'none';
+  } else {
+    prioSmallScreen.style.display = 'none';
+    dueDateSmallScreen.style.display = 'none';
+    categorySmallScreen.style.display = 'none';
+    subTasksSmallScreen.style.display = 'none';
+    bigScreen.style.display = 'block';
   }
+}
+
+function checkforAddTaskPage() {
+  let url = window.location.href; 
+
+  if (url.endsWith('add_task.html')) {
+     document.addEventListener('DOMContentLoaded', function () {
+        showAndHideBoxesAccordingToScreenSize(); // Initial check
+        window.addEventListener('resize', showAndHideBoxesAccordingToScreenSize);
+     });
+  }
+}
+
+checkforAddTaskPage();
+
