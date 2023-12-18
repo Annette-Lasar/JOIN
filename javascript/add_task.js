ef674d69cb20a46890820cd37588dce0619af5f4
Annette-Lasar <a_lasar@web.de>
2023-12-18 23:44:40 +0100

    Add and delete subtasks

--- javascript/add_task.js
+++ javascript/add_task.js
@@ -1,7 +1,10 @@
+let subTasks = [];
+
 function initTasks() {
   init();
   showAndHideBoxesAccordingToScreenSize();
   checkforAddTaskPage();
+  renderSubtasks();
 }
 
 /**
@@ -17,7 +20,6 @@ function toggleDropdownLists(idContainer, idArrow) {
   CATEGORY_List.classList.toggle('show');
   SELECT_ARROW.classList.toggle('turn');
 }
-
 
 function showAndHideBoxesAccordingToScreenSize() {
   let prioSmallScreen = document.getElementById('prio_small_screen');
@@ -43,15 +45,129 @@ function showAndHideBoxesAccordingToScreenSize() {
 }
 
 function checkforAddTaskPage() {
-  let url = window.location.href; 
+  let url = window.location.href;
 
   if (url.endsWith('add_task.html')) {
-     document.addEventListener('DOMContentLoaded', function () {
-        showAndHideBoxesAccordingToScreenSize(); // Initial check
-        window.addEventListener('resize', showAndHideBoxesAccordingToScreenSize);
-     });
+    document.addEventListener('DOMContentLoaded', function () {
+      showAndHideBoxesAccordingToScreenSize(); // Initial check
+      window.addEventListener('resize', showAndHideBoxesAccordingToScreenSize);
+    });
   }
 }
 
 checkforAddTaskPage();
+
+function showCancelAndAcceptSubtask() {
+  const CLOSE_AND_CHECK_WRAPPER = document.getElementById(
+    'close_and_check_wrapper_big'
+  );
+  const SUBTASK_PLUS_BIG = document.getElementById('subtask_plus_big');
+  CLOSE_AND_CHECK_WRAPPER.classList.remove('d-none');
+  SUBTASK_PLUS_BIG.classList.add('d-none');
+}
+
+function clearSubtask() {
+  document.getElementById('sub_tasks_big').value = '';
+  hideCancelAndAcceptSubtask();
+}
+
+function hideCancelAndAcceptSubtask() {
+  const CLOSE_AND_CHECK_WRAPPER = document.getElementById(
+    'close_and_check_wrapper_big'
+  );
+  const SUBTASK_PLUS_BIG = document.getElementById('subtask_plus_big');
+  CLOSE_AND_CHECK_WRAPPER.classList.add('d-none');
+  SUBTASK_PLUS_BIG.classList.remove('d-none');
+}
+
+/* function addSubtask() {
+  const SUBTASK_INPUT_BOX = document.getElementById('sub_tasks_big');
+  const SUBTASK_CONTAINER_BIG = document.getElementById('subtask_container_big');
+  SUBTASK_CONTAINER_BIG.innerHTML = '';
+  let html = `<ul id="subtask_unordered_list_big" class="subtask-unordered-list">`;
+  if (SUBTASK_INPUT_BOX.value !== '') {
+    console.log('Input Value:', SUBTASK_INPUT_BOX.value);
+    subTasks.push(SUBTASK_INPUT_BOX.value);
+  }
+  html += renderSubtasks();
+  html += `</ul>`;
+  SUBTASK_CONTAINER_BIG.innerHTML = html;
+  document.getElementById('sub_tasks_big').value = '';
+  clearSubtask();
+}
+
+function generateSubtaskHTML(i, subtask) {
+  return `
+  <li id="subtask_entry${i}">${subtask}
+    <div class="subtask-list-wrapper">
+      <img src="../icons/edit_dark.svg">
+      <div class="subtask-separator-line"></div>
+      <img onclick="deleteSubtask(${i})" src="../icons/delete.svg">
+    </div>
+  </li>
+  `;
+}
+
+function deleteSubtask(i) {
+  subTasks.splice(i, 1);
+  renderSubtasks();
+}
+
+function renderSubtasks() {
+  const SUBTASK_CONTAINER_BIG = document.getElementById(
+    'subtask_container_big'
+  );
+  SUBTASK_CONTAINER_BIG.innerHTML = '';
+  for (let i = 0; i < subTasks.length; i++) {
+    let subtask = subTasks[i];
+    SUBTASK_CONTAINER_BIG.innerHTML += generateSubtaskHTML(i, subtask);
+  }
+} */
+
+function addSubtask() {
+  const SUBTASK_INPUT_BOX = document.getElementById('sub_tasks_big');
+  const SUBTASK_CONTAINER_BIG = document.getElementById('subtask_container_big');
+  SUBTASK_CONTAINER_BIG.innerHTML = '';
+  
+  if (SUBTASK_INPUT_BOX.value !== '') {
+    console.log('Input Value:', SUBTASK_INPUT_BOX.value);
+    subTasks.push(SUBTASK_INPUT_BOX.value);
+  }
+
+  renderAndInsertSubtasks();
+  document.getElementById('sub_tasks_big').value = '';
+  clearSubtask();
+}
+
+function deleteSubtask(i) {
+  subTasks.splice(i, 1);
+  renderAndInsertSubtasks();
+}
+
+function renderAndInsertSubtasks() {
+  const SUBTASK_CONTAINER_BIG = document.getElementById('subtask_container_big');
+  SUBTASK_CONTAINER_BIG.innerHTML = renderSubtasks();
+}
+
+function generateSubtaskHTML(i, subtask) {
+  return /* html */ `
+    <li id="subtask_entry${i}">${subtask}
+      <div class="subtask-list-wrapper">
+        <img src="../icons/edit_dark.svg">
+        <div class="subtask-separator-line"></div>
+        <img onclick="deleteSubtask(${i})" src="../icons/delete.svg">
+      </div>
+    </li>
+  `;
+}
+
+function renderSubtasks() {
+  let html = `<ul id="subtask_unordered_list_big" class="subtask-unordered-list">`;
+  for (let i = 0; i < subTasks.length; i++) {
+    let subtask = subTasks[i];
+    html += generateSubtaskHTML(i, subtask);
+  }
+  html += `</ul>`;
+  return html;
+}
 
