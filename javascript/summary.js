d44b25672e48331b4d832fa1f4cf9fc568946ad0
JulianHeupgen <julianheupgen@web.de>
2023-12-19 12:50:30 +0100

    added function greetUser()

---
+++ javascript/summary.js
@@ -0,0 +1,13 @@
+function greetUser() {
+    document.getElementById('greeting').innerHTML = 'Good morning,';      // = getDaytime();
+    
+}
+
+
+function getDaytime() {
+
+}
+
+// herausfinden welche Tageszeit; dementsprechend goodMorning/goodEvening etc
+// guestLogin oder angemeldeter User ? Dementsprechend Name bei Greeting (WELCHER User ?) 
+
