cae3cbf7ff2d6177375a2b5382d427c36ad1fc4e
JulianHeupgen <julianheupgen@web.de>
2023-12-19 14:47:34 +0100

    added function getDaytime()

--- javascript/summary.js
+++ javascript/summary.js
@@ -1,13 +1,23 @@
+let userLogin = false;
+
 function greetUser() {
-    document.getElementById('greeting').innerHTML = 'Good morning,';      // = getDaytime();
-    
+    document.getElementById('greeting').innerHTML = getDaytime();  
+    document.getElementById('greeting_name').innerHTML = 'Sofia Müller';   
 }
 
 
 function getDaytime() {
+    let dayTime = new Date().getHours();
 
+    if(dayTime < 12) {
+        return 'Good morning,'
+    } else if (dayTime >= 12 && dayTime < 18) {
+        return 'Good afternoon,'
+    } else {
+        return 'Good evening,'
+    }
 }
 
-// herausfinden welche Tageszeit; dementsprechend goodMorning/goodEvening etc
-// guestLogin oder angemeldeter User ? Dementsprechend Name bei Greeting (WELCHER User ?) 
 
+// guestLogin oder angemeldeter User ? Dementsprechender Name bei Greeting (WELCHER User ?); 
+// wenn GuestLogin kein Name und kein Komma in Begrüßung
