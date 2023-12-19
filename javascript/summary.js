4def4157330dc4aaca5d2856ecaa475404943697
JulianHeupgen <julianheupgen@web.de>
2023-12-19 15:33:40 +0100

    added function getUserName()

--- javascript/summary.js
+++ javascript/summary.js
@@ -2,14 +2,14 @@ let userLogin = false;
 
 function greetUser() {
     document.getElementById('greeting').innerHTML = getDaytime();  
-    document.getElementById('greeting_name').innerHTML = 'Sofia Müller';   
+    document.getElementById('greeting_name').innerHTML = 'Sofia Müller';  // = getUserName();  nur bei UserLogin
 }
 
 
 function getDaytime() {
     let dayTime = new Date().getHours();
 
-    if(dayTime < 12) {
+    if(dayTime < 12) {                 // verschachtelte if-Abfrage: if(userLogin) { ..... } else { return 'Good morning'} (ohne Komma)
         return 'Good morning,'
     } else if (dayTime >= 12 && dayTime < 18) {
         return 'Good afternoon,'
@@ -19,5 +19,10 @@ function getDaytime() {
 }
 
 
-// guestLogin oder angemeldeter User ? Dementsprechender Name bei Greeting (WELCHER User ?); 
-// wenn GuestLogin kein Name und kein Komma in Begrüßung
+function getUserName() {
+
+}
+
+
+// UserLogin: Dementsprechender Name bei Greeting (WELCHER User ?); 
+// GuestLogin: kein Name und kein Komma in Begrüßung
