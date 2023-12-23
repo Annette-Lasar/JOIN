af81a2b53d80a337f10a24f9a55124f2009daecd
JulianHeupgen <julianheupgen@web.de>
2023-12-23 15:00:00 +0100

    comments on functions loginSuccessful() and logout()

--- javascript/script.js
+++ javascript/script.js
@@ -67,8 +67,8 @@ function checkIfSummaryPage() {
 
 /* folgende Function ist nur für erfolgreichen USER-Login, NICHT bei GuestLogin !
 *  Name für key 'userLogin' wurde bewusst gewählt, da genau dieser key in summary.js abgerufen wird ( dort in der Function 'checkLocalStorage()' )
-*  Function loginSuccessful() muss noch richtig implementiert werden in index.html ( vorübergehend erstmal auf dem Login-Button platziert )
-*  Function ist noch nicht final
+*  Function loginSuccessful() muss noch richtig implementiert werden in index.html (vorübergehend zu Testzwecken auf dem Login-Button platziert )
+*  Function ist noch nicht final; zB noch Weiterleitung zu summary.html einbinden
 */
 function loginSuccessful() {
   let successfulLogin = true;
