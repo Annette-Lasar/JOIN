6689606d8b545bf76adf52c3116253728e27db46
JulianHeupgen <julianheupgen@web.de>
2024-01-02 08:03:14 +0100

    added comment

--- javascript/login.js
+++ javascript/login.js
@@ -12,11 +12,6 @@ function login() {
 }
 
 
-/** 
-*  folgende Function ist nur für erfolgreichen USER-Login, NICHT bei GuestLogin !
-*  Name für key 'userLogin' wurde bewusst gewählt, da genau dieser key in summary.js abgerufen wird ( dort in der Function 'checkLocalStorage()' )
-*  Function loginSuccessful() muss noch richtig implementiert werden in index.html (vorübergehend zu Testzwecken auf dem Login-Button platziert )
-*/
 function loginSuccessful(user) {
     let successfulLogin = true;
     successfulLogin = JSON.stringify(successfulLogin);
@@ -26,4 +21,3 @@ function loginSuccessful(user) {
     userName = JSON.stringify(userName);
     localStorage.setItem('userName', userName);
 }
-
